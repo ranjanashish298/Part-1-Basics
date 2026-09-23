2bf39bf5ee5c86574f82536e4f6468072b82dd76
Ashish <s8asranj@stud.uni-saarland.de>
2026-09-23 15:30:28 +0200

    Page 103

--- 1.3-Lists.py
+++ 1.3-Lists.py
@@ -1,23 +1,44 @@
+# Notes
+
+# Variables will contain references to list rather than list values
+# themselves. But for strings and integer values, variables simply contain the
+# string or integer value. Python uses references whenever variables must
+# store values of mutable data types, such as lists or dictionaries. For values
+# of immutable data types such as strings, integers, or tuples, Python vari-
+# ables will store the value itself.
+
+
 #allMyFriends.py
 #Enter names of all your friends in a sorted order! 
 
-nameList = []
+# nameList = []
 
-i=1
+# i=1
 
-while True:
-    print(f"Enter name of your {i} friend")
-    name = input()
-    #nameList = nameList + [name]
-    nameList = nameList.append(name)
-    i = i + 1
+# while True:
+#     print(f"Enter name of your {i} friend")
+#     name = input()
+#     nameList = nameList + [name]
+#     nameList = nameList.append(name)
+#     i = i + 1
 
-    if name == '':
-        break
+#     if name == '':
+#         break
 
 # Sort the names
-newList = sorted(nameList)
+# newList = sorted(nameList)
 
-for names in newList:
-     print(names)
+# for names in newList:
+#      print(names)
 
+
+# Comma Code
+spam = ['apples', 'bananas', 'tofu', 'cats', 4 ]
+def callTheListFunction(givenList):
+
+    for i in range(len(givenList)):
+        if i == len(givenList) - 1 :
+            spam[i] = "and "+ str(givenList[i])
+            
+callTheListFunction(spam)
+print(spam)
